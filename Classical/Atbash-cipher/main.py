992faef36162418b3e5d80063d1f0925d070c9b7
MadhurDixit13 <madhurdixit37@gmail.com>
2022-02-17 23:43:28 +0530

    madhurs commit

---
+++ Classical/Atbash-cipher/main.py
@@ -0,0 +1,28 @@
+lookup_table = {'A' : 'Z', 'B' : 'Y', 'C' : 'X', 'D' : 'W', 'E' : 'V',
+		'F' : 'U', 'G' : 'T', 'H' : 'S', 'I' : 'R', 'J' : 'Q',
+		'K' : 'P', 'L' : 'O', 'M' : 'N', 'N' : 'M', 'O' : 'L',
+		'P' : 'K', 'Q' : 'J', 'R' : 'I', 'S' : 'H', 'T' : 'G',
+		'U' : 'F', 'V' : 'E', 'W' : 'D', 'X' : 'C', 'Y' : 'B', 'Z' : 'A'}
+def atbash(message):
+	cipher = ''
+	for letter in message:
+		if(letter != ' '):
+			cipher += lookup_table[letter]
+		else:
+			cipher += ' '
+	return cipher
+def main():	
+	message = input("Enter the message:")
+	encrypt=atbash(message.upper())
+	print("Encrypted message:",encrypt)
+if __name__ == '__main__':
+	main()
+
+	
+
+
+
+
+
+
+
